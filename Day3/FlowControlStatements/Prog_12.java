70189990acdefec6d621eb28d62e7e39801b3bbc
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-28 20:41:45 +0530

    Create Prog_12.java

---
+++ Day3/FlowControlStatements/Prog_12.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class prog12 { 
+public static void main(String[] args) { 
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt(); 
+	int f=0; 
+	for(int i=2;i<=n/2;i++) { 
+		if(n%i==0) {
+			f=1;
+			break;
+		}		
+	}
+	if(f==0) 
+		System.out.println("Prime"); 
+	else 
+		System.out.println("Not prime"); 
+       }
+}
