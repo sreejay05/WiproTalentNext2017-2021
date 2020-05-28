4a12b2dd2c49542b7222228fd2ee70b5b9f526d2
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-28 19:20:09 +0530

    Update Prog3.java

--- Day3/LanguageBasics/Prog3.java
+++ Day3/LanguageBasics/Prog3.java
@@ -1,6 +1,9 @@
-public class Prog3
-{
-	public static void main(String[] args) {
-		System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-	}
-}
+public class Prog3{  
+    public static void main(String[] args){ 
+	int a,b,sum;
+	a=Integer.parseInt(args[0]);
+	b=Integer.parseInt(args[1]);
+	sum=a+b;
+        	System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+ sum);
+    }  
+}  
