afe067da6847be931fe86043dce7bfb03cc6dd58
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-29 15:35:58 +0530

    Create Prog4.java

---
+++ Day3/Arrays/Prog4.java
@@ -0,0 +1,8 @@
+public class Prog4{
+    public static void main(String[] args) {
+        int arr[]={65,70,75,80,85};
+        for(int  i=0;i<arr.length;i++) {
+                System.out.println((char)arr[i]);
+        }
+    }
+}
