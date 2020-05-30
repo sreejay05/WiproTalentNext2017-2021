4ca4e7d5455f87b1bd726ed709b877bcec0f8c90
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-30 20:02:28 +0530

    Create Person.java

---
+++ 29-05-2020/Inheritance/Prog2/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+	private String name;
+	public Person(String n)
+	{
+		name=n;
+	}
+	public void setName(String n)
+	{
+		name=n;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String toString()
+	{
+		return name;
+	}
+}
