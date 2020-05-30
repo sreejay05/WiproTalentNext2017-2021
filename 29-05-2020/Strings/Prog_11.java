d8a36579d6eecf7cd29e48adbb83ace192bc7fbc
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-30 20:26:47 +0530

    Create Prog_11.java

---
+++ 29-05-2020/Strings/Prog_11.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+public class Prog_11{
+	public static void main(String[] args) {
+		int i,j;
+		Scanner s = new Scanner(System.in);
+		String a=s.next();
+		String b=s.next();
+		for(i=0,j=0;i<a.length()|| j<b.length();i++,j++){
+			if(i<a.length())
+				System.out.print(a.charAt(i));
+			if(j<b.length())
+				System.out.print(b.charAt(j));
+		}
+	}
+}
