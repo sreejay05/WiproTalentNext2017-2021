c72219d6c936b5b446b64596295e2ab75b1bb798
sreejay05 <58815644+sreejay05@users.noreply.github.com>
2020-05-30 20:04:23 +0530

    Create TestEmployee.java

---
+++ 29-05-2020/Inheritance/Prog2/TestEmployee.java
@@ -0,0 +1,8 @@
+public class TestEmployee{
+    public static void main(String[]args){
+        Person p= new Person("John");
+        Employee e=new Employee("Jason",50000, 2020, 1234);
+        System.out.println(p);
+        System.out.println(e);
+    }
+}
